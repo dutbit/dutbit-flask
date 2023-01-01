eadccd31dd0e9c3917f4a62a25eb5934790544de
LiuCY <23275487+ChuckieEsan@users.noreply.github.com>
2023-01-01 15:03:51 +0800

    style: 1. 调整了字体

--- studio/utils/postcard.py
+++ studio/utils/postcard.py
@@ -3,11 +3,11 @@ from manim import *
 
 class PostcardText(Scene):
     def construct(self) -> None:
-        title = Text("{title}", font="宋体", font_size=25)
+        title = Text("{title}", font="Source Han Serif CN", font_size=25)
         title.set_width(2.2)
         title.set_max_height(0.5)
         title.set_color(RED_B).set_x(3.95).set_y(0.75)
-        content = Text("{content}", font="宋体", font_size=16, color=GREY_D)
+        content = Text("{content}", font="Source Han Serif CN", font_size=16, color=GREY_D, line_spacing=0.3)
         content.set_width(2.2)
         content.next_to(title, DOWN)
         img = ImageMobject("{img_url}")
